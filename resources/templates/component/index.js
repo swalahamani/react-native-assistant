fb14facbff7937158be18f0f1a59b4bde7fd032c
Muhammad Swalah A A <mail@swalahamani.com>
2022-08-21 19:53:16 +0530

    chore: delete stale implementation

--- resources/templates/component/index.js
+++
@@ -1,9 +0,0 @@
-/******************************************
- *  Author : [AUTHOR]  
- *  Created On : [DATE_AND_TIME]
- *  File : [FILE]
- *******************************************/
-"use strict";
-import [COMPONENT_NAME] from './[COMPONENT_NAME]';
-
-module.exports = [COMPONENT_NAME];
